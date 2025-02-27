56fff860fc042cdf5eae77959403e9d70641bec5
nositno4 <28894906+nositno4@users.noreply.github.com>
2025-02-28 06:50:48 +0800

    调整窗口大小

--- WebTimeSnip.py
+++ WebTimeSnip.py
@@ -11,17 +11,18 @@ def create_screenshot_taker():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(service=Service(), options=options)
-    driver.set_window_size(1920, 1080*2)
+    driver.set_window_size(1000, 2000)
 
     def take_screenshot(url, output_path, wait_time=3):
         try:
             os.makedirs(output_path, exist_ok=True)
             driver.get(url)
             time.sleep(wait_time)
+            # 滚动一次滚轮, 去掉挡在上面的登录框
             driver.execute_script(f"window.scrollBy(0, 120);")  # 向下滚动
-            time.sleep(0.5)  # 添加一个短暂的延迟，以便观察滚动效果
+            time.sleep(0.5)  
             driver.execute_script(f"window.scrollBy(0, -120);") # 向上滚动
-            time.sleep(0.5)  # 添加一个短暂的延迟，以便观察滚动效果
+            time.sleep(0.5)  
 
 
             timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
